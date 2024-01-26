009a80e464b8c0f5489b4ac3fd69dcaaef7e3d76
wangyifan349 <86041125+wangyifan349@users.noreply.github.com>
2024-01-26 19:24:33 +0800

    Update vps无脑脚本.sh

--- vps无脑脚本.sh
+++ vps无脑脚本.sh
@@ -3,4 +3,5 @@ sudo apt install software-properties-common
 sudo add-apt-repository ppa:ondrej/php
 sudo apt update
 sudo apt install php7.4 php7.4-fpm
-
+sudo systemctl start php7.4-fpm
+sudo systemctl enable php7.4-fpm
