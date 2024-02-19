0ebb0df6e080dbe248f447f5d6be01bca999a4d4
wangyifan349 <86041125+wangyifan349@users.noreply.github.com>
2024-02-19 08:43:53 +0800

    Create 编程环境.py

---
+++ 编程环境.py
@@ -0,0 +1,17 @@
+   sudo apt install gcc
+   
+   sudo apt install g++
+   
+   sudo apt install python3
+   
+   sudo apt install python3-pip
+   
+   sudo apt install default-jdk
+   
+   sudo apt install nodejs
+   
+   sudo apt install npm
+   
+   sudo apt install git
+   
+pip install Pillow requests  opencv-python urlllib3
