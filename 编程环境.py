2aee0e71145c5f3fe4d79db2d5925a6239b85b16
wangyifan349 <86041125+wangyifan349@users.noreply.github.com>
2024-02-21 12:09:13 +0800

    Update 编程环境.py

--- 编程环境.py
+++ 编程环境.py
@@ -11,7 +11,14 @@
    sudo apt install nodejs
    
    sudo apt install npm
+sudo npm install -g typescript
+
+
    
    sudo apt install git
    
 pip install Pillow requests  opencv-python urlllib3
+
+
+apt install build-essential cmake pkg-config libssl-dev libzmq3-dev libunbound-dev libsodium-dev libunwind8-dev liblzma-dev libreadline6-dev libexpat1-dev libpgm-dev qttools5-dev-tools libhidapi-dev libusb-1.0-0-dev libprotobuf-dev protobuf-compiler libudev-dev libboost-chrono-dev libboost-date-time-dev libboost-filesystem-dev libboost-locale-dev libboost-program-options-dev libboost-regex-dev libboost-serialization-dev libboost-system-dev libboost-thread-dev python3 ccache doxygen graphviz
+apt install libevent-dev libminiupnpc-dev libnatpmp-dev libsqlite3-dev
